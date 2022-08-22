d2a2b5d910972bd6b2f9477294d00e0de1dec1b8
Seanghay Yath <seanghay.dev@gmail.com>
2022-08-22 15:50:03 +0700

    docs: add examples

---
+++ examples/fetch.js
@@ -0,0 +1,14 @@
+(async function () {
+  const { promisive } = require('..');
+  const axios = require('axios').default;
+  const f = url => axios.get(url).then(d => d.data);
+  const response = await promisive({
+    posts: f('https://jsonplaceholder.typicode.com/posts'),
+    a: [
+      { 
+        users: f('https://jsonplaceholder.typicode.com/users'),
+      }
+    ]
+  })
+  console.log(response)
+})()
